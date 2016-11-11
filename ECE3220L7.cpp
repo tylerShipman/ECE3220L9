5b679f4f6cecea61a9145e501cf9abfe20e9dc9a
Tyler Shipman <TylerShipman@mwc-064049.dhcp.missouri.edu>
2016-11-11 17:21:09 -0600

    Updated

--- ECE3220L7.cpp
+++ ECE3220L7.cpp
@@ -1,118 +1,83 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * File:   main.cpp
- * Author: TylerShipman
- *
- * Created on October 13, 2016, 10:37 AM
- */
-
 #include <cstdlib>
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+
 #include <vector>
 #include <string>
+
 using namespace std;
 
-//double findAverage(double* values, int size); //Finds average of data
-double findAverage(vector<double> values);
+
+
 class LabFunction {	//stores information about file
 
 private:
 
-	//average of file
+	int maximumValue;	//max possible value in file
+	double average;		//average of file
 	//double *data;		//data from file
 
 public:
-	int maximumValue;	//max possible value in file
-	double average;
-	int length;			//number of values in file
 	vector<double> data;
+	int length;			//number of values in file
 	LabFunction();
 	LabFunction(int);	//takes a number and opens the corresponding file
-	LabFunction(string);	//takes a filename and opens the file
+	//LabFunction(char[20]);	//takes a filename and opens the file
+	LabFunction(string);
 	~LabFunction();		//frees data
 
-	void offset(double);	//replaces data with offset data
+	void offset(int);	//replaces data with offset data
 	void scale(int);		//replaces data with scale data
 	void normalize();	//replaces data with normalize data
 	void center();		//replaces data with center data
 
 	void Sig_info();		//prints info to terminal
 	void Save_file();	//saves info to file
+	void setAvg();
+	void setMax();
+
 
 	void operator+(double offset){
-		for(int i = 0; i < length; i++)
-			data[i] = data[i] + (double) offset;
-
+		for (int i = 0; i < length; i++)
+			data[i] += offset;
+	}
+	void operator*(double scale){
+		for (int i = 0; i < length; i++)
+			data[i] *= scale;
 	}
 
-	void operator*(int factor){
-
-		    for(int i=0;i<length;i++)
-		    		data[i] *= (double)factor;
-
-
-	}
-
-	vector<double> getData() { //Getter that returns data
-		return data;
-	}
 
 };
 
-LabFunction operator+(const LabFunction &x , const LabFunction &y){
-	if(x.length != y.length)
-		{
-			cout << "Error: objects need to be of the same length." << endl;
-			exit(-1);
-		}
-		else
-		{
-			LabFunction sum;
-			sum.length = x.length;
-			for(int i=0; i<sum.length; i++)
-			{
-				// use push_back to grow vector
-				sum.data.push_back(x.data[i]);
-				sum.data[i] += y.data[i];
-			}
-			sum.average = findAverage(sum.data);
-
-			return sum;
-		}
-
-}
-
 LabFunction::LabFunction(){
 
-	data.resize(0);
+	length = 0;
 	maximumValue = 0;
 	average = 0;
-	length = 0;
+
+
+
 }
 
 LabFunction::LabFunction(int userChoice) {
 
-	char inputFileName[20];
+	//char inputFileName[20];
+	string inputFileName;
 	FILE* inputFp;
 	int sizeOfFile, maxOfFile, i;
 	double dataOfFile;
 	if (userChoice < 10) {
-		sprintf(inputFileName, "Raw_data_0%d.txt", userChoice);
-
+		//sprintf(inputFileName, "Raw_data_0%d.txt", userChoice);
+		inputFileName = "Raw_data_0" + to_string(userChoice) + ".txt";
 	} else {
-		sprintf(inputFileName, "Raw_data_%d.txt", userChoice);
+		//sprintf(inputFileName, "Raw_data_%d.txt", userChoice);
+		inputFileName = "Raw_data_"+ to_string(userChoice) + ".txt";
 	}
 	cout<<inputFileName<<endl;
-	inputFp = fopen(inputFileName, "r");
+	inputFp = fopen(inputFileName.c_str(), "r");
 	if (!inputFp) {
 		printf("Error opening input file.");
 	}
@@ -123,21 +88,19 @@ LabFunction::LabFunction(int userChoice) {
 	maximumValue = maxOfFile;
 	cout << maxOfFile << endl;
 	//data = new double[length];
-	data.resize(length);
-
 	for (i = 0; i < sizeOfFile; i++) {
 
 		fscanf(inputFp, "%lf", &dataOfFile);
 		data[i] = dataOfFile;
 	}
 
-	for (i = 0; i < sizeOfFile; i++) {
-
-		cout << data[i] << endl;
-	}
+//	for (i = 0; i < sizeOfFile; i++) {
+//
+//		cout << data[i] << endl;
+//	}
 
 	//average = findAverage(getData(), length);
-	average = findAverage(getData());
+	fclose(inputFp);
 
 }
 
@@ -156,13 +119,13 @@ LabFunction::LabFunction(string fileName) {
 	maximumValue = maxOfFile;
 
 	//data = new double[sizeOfFile];
-	data.resize(length);
+
 
 	for (i = 0; i < sizeOfFile; i++) {
 
 		fscanf(inputFp, "%lf", &dataOfFile);
 
-		data[i] = dataOfFile;
+		data.push_back(dataOfFile);
 	}
 
 	for (i = 0; i < sizeOfFile; i++) {
@@ -171,8 +134,12 @@ LabFunction::LabFunction(string fileName) {
 
 	}
 
-	//average = findAverage(data, length);
-	average = findAverage(getData());
+
+
+	fclose(inputFp);
+
+
+
 
 }
 
@@ -181,25 +148,16 @@ LabFunction::~LabFunction() {
 }
 
 void LabFunction::scale(int scale) {
-//	int i;
-//	for (i = 0; i < length; i++) {
-//
-//		data[i] = data[i] * scale;
-//
-//	}
-
 	*this*scale;
+	maximumValue *= scale;
+	setAvg();
 }
 
-void LabFunction::offset(double offset) {
-//	int i;
-//	for (i = 0; i < length; i++) {
-//
-//		data[i] = data[i] + offset;
-//
-//	}
+void LabFunction::offset(int offset) {
 
 	*this+offset;
+	maximumValue += offset;
+	setAvg();
 
 }
 
@@ -211,6 +169,8 @@ void LabFunction::center() {
 		data[i] -= average;
 
 	}
+	setAvg();
+	maximumValue-= average;
 
 }
 
@@ -222,13 +182,15 @@ void LabFunction::normalize() {
 		data[i] = (data[i] / (double) maximumValue);
 
 	}
+	setAvg();
+	maximumValue = 1;
 
 }
 
 void LabFunction::Sig_info() {
 	int i;
 	cout << "Length: " << length << endl << "Max Value: " << maximumValue
-			<< endl << "Average: " << average << endl;
+			<< endl<< "Average: " << average << endl;
 	for (i = 0; i < length; i++) {
 		cout << data[i] << endl;
 	}
@@ -249,56 +211,40 @@ void LabFunction::Save_file() {
 
 }
 
-double findAverage(vector<double> values) {
+void LabFunction::setAvg() {
 
-	int i = 0;
 	double sum = 0;
-	double average = 0;
-	for (i = 0; i < values.size(); i++) {
 
-		sum += values[i];
+	for (int i = 0; i < data.size(); i++) {
 
-	}
-
-	average = sum / (double) values.size();
-
-	return average;
-
-}
-
-
-
-void functionEvaluater(LabFunction test, int choice) {	//Works with menu() and performs corresponding operation
-	int factor;
-	if (choice == 1) {
-		cout << "Enter a number to scale by:" << endl;
-		cin >> factor;
-		test.scale(factor);
+		sum += data[i];
 
 	}
-	if (choice == 2) {
-		cout << "Enter a number to offset by: " << endl;
-		cin >> factor;
-		test.offset(factor);
-	}
-	if (choice == 3) {
-		test.center();
-	}
-	if (choice == 4) {
-		test.normalize();
-	}
+
+	average = sum / (double) data.size();
 
 
 
 }
 
-void menu(LabFunction test) {	//Displays the following options and takes the userSeletion
-	int userChoice;
+void LabFunction::setMax(){
+	maximumValue = data[0];
+
+		for(int i=0; i<length; i++)
+		{
+			if( data[i] > maximumValue )
+			{
+				maximumValue = data[i];
+			}
+		}
+
+}
+
+void menu() {	//Displays the following options and takes the userSeletion
+
 	cout << "Select one of the following:" << endl << "1. Scale" << endl
 			<< "2. Offset" << endl << "3. Center" << endl << "4. Normalize"
 			<< endl;
-	cin >> userChoice;
-	functionEvaluater(test, userChoice);
 
 }
 
@@ -322,20 +268,44 @@ void menu2(LabFunction test) { //Displays the following options and takes the us
 
 }
 
+LabFunction operator+(const LabFunction &lhs, const LabFunction &rhs){
+	if(lhs.length != rhs.length)
+		{
+			cout << "Error: objects are not of same length" << endl;
+			exit(-1);
+		}
+		else
+		{
+			LabFunction sum;
+			sum.length = lhs.length;
+			for(int i=0; i<sum.length; i++)
+			{
+				// use push_back to grow vector
+				sum.data.push_back(lhs.data[i]);
+				sum.data[i] += rhs.data[i];
+			}
+			sum.setAvg();
+			sum.setMax();
+			return sum;
+		}
+}
+
 int main(int argc, char** argv) {
 
 	int counter = 1;			//Keeps track of args
 	int userChoice;			//takes user input
-	char inputFileName[30];	//name of file
-
-
+	//char inputFileName[30];	//name of file
+	string inputFileName;
+	int selection;
+	int factor;
 
 
 	if (argc == 3) {
 
 		if (strcmp(argv[counter], "-n") == 0) {
 			LabFunction test(atoi(argv[counter+1]));
-			menu(test);
+			menu();
+			cin>>selection;
 			menu2(test);
 
 
@@ -343,7 +313,8 @@ int main(int argc, char** argv) {
 		} else if (strcmp(argv[counter], "-f") == 0) {
 
 			LabFunction test(argv[counter + 1]);
-			menu(test);
+			menu();
+			cin>>selection;
 			menu2(test);
 
 		}
@@ -359,23 +330,49 @@ int main(int argc, char** argv) {
 		}
 
 		if (userChoice < 10) {
-			sprintf(inputFileName, "Raw_data_0%d.txt", userChoice);
+			//sprintf(inputFileName, "Raw_data_0%d.txt", userChoice);
+			inputFileName = "Raw_data_0" + to_string(userChoice) + ".txt";
 
 		} else {
-			sprintf(inputFileName, "Raw_data_%d.txt", userChoice);
+			//sprintf(inputFileName, "Raw_data_%d.txt", userChoice);
+			inputFileName = "Raw_data_" + to_string(userChoice) + ".txt";
 		}
 
 
 		LabFunction test(inputFileName);
 
-		menu(test);
+		menu();
+		cin>>selection;
+		if (selection == 1) {
+					cout << "Enter a number to scale by:" << endl;
+					cin >> factor;
+					test.scale(factor);
+
+				}
+				if (selection == 2) {
+					cout << "Enter a number to offset by: " << endl;
+					cin >> factor;
+					test.offset(factor);
+				}
+				if (selection == 3) {
+					test.center();
+				}
+				if (selection == 4) {
+					test.normalize();
+				}
+
 		menu2(test);
 
 
 	}
 
+	LabFunction function3("Raw_data_01.txt");
+	cout<<"HERE"<<endl;
+	LabFunction function2("Raw_data_01.txt");
+	LabFunction function4 = operator+(function2,function3);
+	function4.Sig_info();
+
 
 
 	return 0;
 }
-
